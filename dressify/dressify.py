26c75bc3467a702067e27d4820847fd421f69273
Sophia <sofaz2214@gmail.com>
2023-10-28 13:34:05 -0700

    message

--- dressify/dressify.py
+++ dressify/dressify.py
@@ -9,19 +9,46 @@ filename = f"{config.app_name}/{config.app_name}.py"
 
 class State(rx.State):
     """The app state."""
-
-    pass
+    def onclick(self):
+        return rx.redirect("http://google.com")
 
 def index() -> rx.Component:
     return rx.fragment(
         rx.color_mode_button(rx.color_mode_icon(), float="right"),
     
-         rx.image(
-            src="box 1.png",
-            width="100%",
-            height="100%"
-        ),
+        
         rx.vstack(
+
+            rx.link(
+                "Wardrobe",
+                href=docs_url,
+                color="#FBFBFB",
+                font_size="1em",
+                font_family="static/Raleway-Light.ttf",
+                font_weight=400,
+                word_wrap="break-word",
+                display="flex",
+                justify_content="center",
+                align_items="center",
+                margin_top="1em",
+            ),
+            
+            rx.hstack(
+                rx.image(
+            src="box 1.png",
+            width= "30%",
+            margin_top= "3em",
+                ),
+            rx.image(
+                src="box2.png",
+                width= "30%",
+                ),
+            rx.image(
+                src="box3.png",
+                width= "30%",
+                margin_top= "3em",
+                ),
+            ),
             rx.text(
                 "Dressify",
                 color="#FBFBFB",
@@ -30,11 +57,49 @@ def index() -> rx.Component:
                 font_weight=400,
                 word_wrap="break-word"
             ),
-            rx.heading("Welcome to REFLEX!", font_size="2em"),
+
+            rx.button(
+                "START",
+                on_click= State.onclick
+            ),
+
+            rx.text(
+                "GETTING STARTED",
+                width= "100%",
+                height= "100%",
+                color= "#FBFBFB", 
+                font_size="1em", 
+                font_family="static/Raleway-Light.ttf",
+                font_weight= 400,
+                word_wrap="break-word",
+                display="flex",
+                justify_content="center",
+                align_items="center",
+            ),
             
-            rx.box("Begin by editing ", rx.code(filename, font_size="1em")),
+            rx.hstack(
+                rx.box(
+                    width="30%",
+                    height="100px",
+                    background= "#BCABAE",
+                    border_radius="218.50px",
+                ),
+                rx.box(
+                    width="30%",
+                    height="100px",
+                    background= "#FBFBFB",
+                    border_radius="218.50px",
+                ),
+                rx.box(
+                    width="30%",
+                    height="100px",
+                    background= "#716969",
+                    border_radius="218.50px",
+                ),
+                ),
+
             rx.link(
-                "gremlin!",
+                "+ add items",
                 href=docs_url,
                 border="0.1em solid",
                 padding="0.5em",
@@ -49,6 +114,8 @@ def index() -> rx.Component:
             spacing="1.5em",
             font_size="2em",
             padding_top="10%",
+            background= "linear-gradient(180deg, #050303 0%, #2D2E2E 100%)",
+            background_size= "100%",
         ),
     )
 
